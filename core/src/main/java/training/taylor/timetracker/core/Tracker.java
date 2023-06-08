87f992f76fcfa36b7b69f45390e485c6f81c4cea
psvadlani <126697352+psvadlani@users.noreply.github.com>
2023-06-08 10:49:49 -0500

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -26,7 +26,7 @@ public class Tracker {
         return entries.size();
     }
 
-    public TimeEntry get(int index) {
+   // public TimeEntry get(int index) {
         try {
 
         } catch (Exception e) {
